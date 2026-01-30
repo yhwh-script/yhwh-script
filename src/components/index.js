576423e0e728b192b4847da4d10b30dd5413ff58
J4H20M3 <rome@live.com>
2026-01-30 01:54:43 +0100

    fix: sqlite depency for build

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export const htmlFiles=[
+  "components/error/error-404.html",
+  "components/router/router-app.html",
+  "components/router/router-hamburger.html",
+  "components/router/router-nav.html",
+  "components/sqlite/sqlite-control.html"
+];
