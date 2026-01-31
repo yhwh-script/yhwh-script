b5e57093ecb51b52585990ce6aecb5d6ac3b35a9
J4H20M3 <rome@live.com>
2026-01-31 18:59:46 +0100

    fixed css for hamburger and menu

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export const htmlFiles=[
   "components/project/project-execute.html",
   "components/project/project-monitor.html",
   "components/project/project-plan.html",
+  "components/project/project-rules.html",
+  "components/project/project-templates.html",
   "components/router/router-app.html",
   "components/router/router-hamburger.html",
   "components/router/router-nav.html",
